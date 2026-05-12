ad00ee6a9b2ea7967747b28e3418a5d9acb75021
Michel <rhawanjs@gmail.com>
2026-05-12 17:59:59 -0300

    Busca de Filmes pela API

---
+++ CineApp-master/Documents/CineApp/src/app/pages/search/search.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonSearchbar, IonButton, IonButtons } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.page.html',
+  styleUrls: ['./search.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonSearchbar, IonButton, IonButtons, CommonModule, FormsModule, RouterLink]
+})
+export class SearchPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
